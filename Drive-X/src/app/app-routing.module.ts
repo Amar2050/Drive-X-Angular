8ffd543cdf54731a17dbb0c7e65939c407e8438d
Amar2050 <amarboutayeb@gmail.com>
2020-10-13 16:49:00 +0200

    Stage 2
    Ajout de component homePage et 404
    Ajout du routing et des mécaniques SPA

--- Drive-X/src/app/app-routing.module.ts
+++ Drive-X/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CarsListComponent } from './cars-list/cars-list.component';
+import { DriversListComponent } from './drivers-list/drivers-list.component';
+import { FourOhfourComponent } from './four-ohfour/four-ohfour.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomePageComponent},
+  {path: 'cars', component: CarsListComponent},
+  {path: 'drivers', component: DriversListComponent},
+  {path: 'not-found', component: FourOhfourComponent},
+  {path: '**', redirectTo: 'not-found'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Drive-X/src/app/app.module.ts
+++ Drive-X/src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { CarsListComponent } from './cars-list/cars-list.component';
 import { DriversListComponent } from './drivers-list/drivers-list.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { FourOhfourComponent } from './four-ohfour/four-ohfour.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     CarsListComponent,
-    DriversListComponent
+    DriversListComponent,
+    HomePageComponent,
+    FourOhfourComponent
   ],
   imports: [
     BrowserModule,
