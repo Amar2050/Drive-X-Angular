075454ee65790de2fedd323360927a9efcd8859f
Amar2050 <amarboutayeb@gmail.com>
2020-10-13 16:19:46 +0200

    Stage 1
    intégration de bootstrap en locale
    Component header pour la page d'accueil,
    component carsList et driversList
    Avant le rooting

--- Drive-X/src/app/app.component.ts
+++ Drive-X/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Drive-X';
+  title = 'Hello World';
 }
--- Drive-X/src/app/app.module.ts
+++ Drive-X/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { CarsListComponent } from './cars-list/cars-list.component';
+import { DriversListComponent } from './drivers-list/drivers-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CarsListComponent,
+    DriversListComponent
   ],
   imports: [
     BrowserModule,
