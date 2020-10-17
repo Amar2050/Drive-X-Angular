7d686949db01e433df98db2e61869e760f51080e
Amar2050 <amarboutayeb@gmail.com>
2020-10-17 21:23:04 +0200

    Retour au STAGE 3
    projet avant utilisation de directive structurelle et attribut
    Nettoyage

--- Drive-X/src/app/drivers-list/drivers-list.component.ts
+++ Drive-X/src/app/drivers-list/drivers-list.component.ts
@@ -7,24 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DriversListComponent implements OnInit {
   
-  // on déclare une propriété 
-  isCentred:boolean;
 
   constructor() { }
 
   ngOnInit(): void {
   }
-
-  // notre méthode retourne un booléen 
-  onClick():boolean {
-    // Si la propriété est vrai on la rend fausse et vice versa
-    // 
-    if(this.isCentred){
-      return this.isCentred= false;
-    }else{
-      return this.isCentred = true;
-    }
-  };
 
 
 }
