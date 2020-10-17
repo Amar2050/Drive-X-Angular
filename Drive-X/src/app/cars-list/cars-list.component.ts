3f81255e7b25b2509c3811c21fe17d0fa0b917b0
Amar2050 <amarboutayeb@gmail.com>
2020-10-17 21:29:56 +0200

    1er ngFor du projet - Utilisation d'une directive structurelle
    Utilisation de ngFor pour régler le problème de répétition du component app-car-item

--- Drive-X/src/app/cars-list/cars-list.component.ts
+++ Drive-X/src/app/cars-list/cars-list.component.ts
@@ -9,24 +9,27 @@ export class CarsListComponent implements OnInit {
 
   carUpdate:any;
 
-    carOne: any = {
+cars:any = 
+[  
+     {
     name: "pagani huayra",
     pays: "italie",
     power: 765,
     perf: 3.2
-  };
-  carTwo: any = {
+  },
+  {
     name: "koenigsegg agera rs",
     pays: "suède",
     power: 1383,
     perf: 2.6
-  };
-  carThree: any = {
+  },
+  {
     name: "zenvo tsr s",
     pays: "danemark",
     power: 1200,
     perf: 2.8
-  };
+  }
+];
 
   constructor() { }
 
