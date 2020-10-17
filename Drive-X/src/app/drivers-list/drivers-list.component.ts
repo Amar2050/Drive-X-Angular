02727db1f91ac7f5cb3d6b6d2b9a8d6ae0d4bd37
Amar2050 <amarboutayeb@gmail.com>
2020-10-17 20:49:05 +0200

    Directive - Directive Attribut- [style.property] - Exemple 1
    La directive style.property permet de lié une propriété css a un element du dom et à la classe. Plus de détails dans le code et commentaire , ainsi que la doc pour aller plus loin

--- Drive-X/src/app/drivers-list/drivers-list.component.ts
+++ Drive-X/src/app/drivers-list/drivers-list.component.ts
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DriversListComponent implements OnInit {
   
-// Je déclare un tableau classique
- fruits:string[] = ["banane","pomme","fraise","peche"];
+  // Je déclare une variable pour l'utilisé avec la directive d'attribut 
+  // style.propriete
+  couleur:string = "blue";
 
+  // Je peux aussi utilisé une méthode avec ma directive d'attribut
+  getColor(){
+    return "aqua";
+  }
+
+  // Je déclare un booléen servant de déclencheur
+  // On peut imaginer un bouton pour un mode nuit
+  flag:boolean = true;
+
+  colorA:string = "green";
+  colorB:string = "grey";
 
   constructor() { }
 
